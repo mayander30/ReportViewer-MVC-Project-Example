3c59e8b1250daf42cb42e2c84d48e43531746dc2
mcaetano <>
2018-09-11 14:58:53 -0300

    first commit

---
+++ pTesteReportView_MVC/Controllers/HomeController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.Reporting.WebForms;
+using pTesteReportView_MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Security;
+using System.Security.Permissions;
+using System.Security.Policy;
+using System.Web;
+using System.Web.Mvc;
+using System.Windows.Forms;
+
+namespace pTesteReportView_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult RDados(System.Web.Mvc.FormCollection form)
+        {
+            ViewBag.Title_Relatorio = "Dados da Lista";
+
+            var reportViewer = ConfigReportViewer();
+            
+            reportViewer.LocalReport.ReportPath = Request.MapPath(Request.ApplicationPath) + @"\Reports\rDados.rdlc";
+
+            System.Windows.Forms.BindingSource source = new BindingSource();
+            source.DataSource = new DadosReport().getLstDados();
+
+            ReportDataSource datasource = new ReportDataSource("Dados", source);
+            reportViewer.LocalReport.DataSources.Add(datasource);
+
+            ViewBag.ReportViewer = reportViewer;
+
+            return View("~/Views/Report/ViewReport.cshtml");
+        }
+
+        public static Microsoft.Reporting.WebForms.ReportViewer
+            ConfigReportViewer()
+        {
+            var reportViewer = new Microsoft.Reporting.WebForms.ReportViewer();
+            reportViewer.ProcessingMode = Microsoft.Reporting.WebForms.ProcessingMode.Local;
+
+            reportViewer.ViewStateMode = System.Web.UI.ViewStateMode.Inherit;
+            reportViewer.SizeToReportContent = true;
+            reportViewer.Width = System.Web.UI.WebControls.Unit.Percentage(110);
+            reportViewer.Height = System.Web.UI.WebControls.Unit.Percentage(100);
+            reportViewer.AsyncRendering = true;
+            reportViewer.ZoomMode = Microsoft.Reporting.WebForms.ZoomMode.FullPage;
+            reportViewer.BackColor = System.Drawing.ColorTranslator.FromHtml("#FFFBF7");
+            reportViewer.ShowPrintButton = true;
+            reportViewer.ShowRefreshButton = false;
+
+            reportViewer.ShowZoomControl = true;
+
+            PermissionSet permissions = new PermissionSet(PermissionState.None);
+            permissions.AddPermission(new FileIOPermission(PermissionState.Unrestricted));
+            permissions.AddPermission(new SecurityPermission(SecurityPermissionFlag.Execution));
+
+            reportViewer.LocalReport.SetBasePermissionsForSandboxAppDomain(permissions);
+
+            Assembly asm = Assembly.Load(Assembly.GetExecutingAssembly().FullName);
+            AssemblyName asm_name = asm.GetName();
+            reportViewer.LocalReport.AddFullTrustModuleInSandboxAppDomain(new StrongName(new StrongNamePublicKeyBlob(asm_name.GetPublicKeyToken()), asm_name.Name, asm_name.Version));
+
+            return reportViewer;
+
+        }
+    }
+}
---
+++ pTesteReportView_MVC/Models/DadosReport.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace pTesteReportView_MVC.Models
+{
+
+    public class DadosReport
+    {
+        public int ID { get; set; }
+        public String DESCRICAO { get; set; }
+
+        public DadosReport()
+        {
+
+        }
+
+        public List<DadosReport> getLstDados()
+        {
+            List<DadosReport> lstDados = new List<DadosReport>();
+
+            for (int i = 0; i < 100; i++)
+            {
+                DadosReport dados = new DadosReport();
+                dados.ID = i;
+                dados.DESCRICAO = " DESCRICAO ITEM - " + i;
+                lstDados.Add(dados);
+            }
+
+            return lstDados;
+        }
+    }
+}
